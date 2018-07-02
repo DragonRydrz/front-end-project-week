0e9e181d9f3cfc7ff50964cf8f1e02675b28fdac
AJ Genung <DragonRydrz@me.com>
2018-07-01 20:30:33 -0400

    updated host to live server

--- src/actions/authorize.js
+++ src/actions/authorize.js
@@ -3,7 +3,7 @@ import { LOGIN } from './login';
 
 export const authorize = data => dispatch => {
   axios
-    .get('https://ajlnbe.herokuapp.com/api/login', data)
+    .get('http://159.89.34.14/api/login', data)
     .then(response => {
       dispatch({ type: LOGIN, payload: response.data.user });
       // this.props.authorize(user);
--- src/actions/editNote.js
+++ src/actions/editNote.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 export const EDIT_NOTE = 'EDIT_NOTE';
 
-const host = 'https://ajlnbe.herokuapp.com/api';
+const host = 'http://159.89.34.14';
 
 export const editNote = data => dispatch => {
   axios
-    .put(`${host}/updatenote`, data, {
+    .put(`${host}/api/updatenote`, data, {
       headers: { Authorization: `Bearer ${localStorage.getItem('Dragons!')}` },
     })
     .then(response => {
